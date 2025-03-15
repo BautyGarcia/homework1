9f46b4d1d088918f46f33e350a7ab7bacf2add2b
Bauty Garcia <bautistaroccogarcia@gmail.com>
2025-03-15 14:08:43 -0300

    y seguimos con las correcciones de marianito

--- ej2/main.cpp
+++ ej2/main.cpp
@@ -5,69 +5,72 @@
 
 using namespace std;
 
-int main()
-{
-    try 
-    {
-        while (true)
-        {
-            string message;
-            string username;
+int main() {
+    try {
+        while (true) {
             int level;
 
-            cout << DEBUG << " - DEBUG" << endl;
-            cout << INFO << " - INFO" << endl;
-            cout << WARNING << " - WARNING" << endl;
-            cout << ERROR << " - ERROR" << endl;
-            cout << CRITICAL << " - CRITICAL" << endl;
-            cout << SECURITY << " - SECURITY" << endl;
+            cout << DEBUG << " - " << LOG_LEVEL_STRINGS[DEBUG] << endl;
+            cout << INFO << " - " << LOG_LEVEL_STRINGS[INFO] << endl;
+            cout << WARNING << " - " << LOG_LEVEL_STRINGS[WARNING] << endl;
+            cout << ERROR << " - " << LOG_LEVEL_STRINGS[ERROR] << endl;
+            cout << FATAL << " - " << LOG_LEVEL_STRINGS[FATAL] << endl;
+            cout << CRITICAL << " - " << LOG_LEVEL_STRINGS[CRITICAL] << endl;
+            cout << SECURITY << " - " << LOG_LEVEL_STRINGS[SECURITY] << endl;
             cout << "-1 - SALIR" << endl;
             cout << "Ingrese el tipo de log: ";
 
             cin >> level;
 
-            if (level == -1)
-            {
+            if (level == -1) {
                 break;
             }
 
-            if (level < 0 || level > LOG_LEVEL_STRINGS->length()) {
+            if (level < 0 || level >= static_cast<int>(LogLevel::COUNT)) {
                 cout << "Nivel de log invalido" << endl;
                 continue;
             }
 
-            switch (level)
-            {
-                case ERROR:
+            switch (level) {
+                case FATAL:
                     throw runtime_error("Algo paso, algo malo seguro");
                     break;
                 case SECURITY:
-                    cout << "Ingrese el mensaje de acceso: ";
-                    cin.ignore();
-                    getline(cin, message);
-                    cout << "Ingrese el nombre de usuario: ";
-                    getline(cin, username);
-                    logMessage(message, username);
+                    handleSecurity();
                     break;
                 default:
-                    cout << "Ingrese el mensaje: ";
-                    cin.ignore();
-                    getline(cin, message);
-                    logMessage(message, static_cast<LogLevel>(level));
+                    handleMessage(static_cast<LogLevel>(level));
                     break;
             }
         }
     }
-    catch (const exception& e)
-    {
+    catch (const exception &e) {
         // No puse un ejemplo pero funciona, si queres registrar un error a voluntad, usa logMeesage pero no corta el programa
         logMeesage(e.what(), __FILE__, __LINE__);
         return 1;
     }
 }
 
-void logMessage(string message, LogLevel level)
-{
+void handleSecurity() {
+    string message;
+    string username;
+    cout << "Ingrese el mensaje de acceso: ";
+    cin.ignore();
+    getline(cin, message);
+    cout << "Ingrese el nombre de usuario: ";
+    getline(cin, username);
+    logMessage(message, username);
+}
+
+void handleMessage(LogLevel level) {
+    string message;
+    cout << "Ingrese el mensaje: ";
+    cin.ignore();
+    getline(cin, message);
+    logMessage(message, static_cast<LogLevel>(level));
+}
+
+void logMessage(string message, LogLevel level) {
     fstream logs;
     logs.open("logs.txt", ios::out | ios::app);
 
@@ -75,8 +78,7 @@ void logMessage(string message, LogLevel level)
     logs.close();
 }
 
-void logMessage(string message, string username)
-{
+void logMessage(string message, string username) {
     fstream logs;
     logs.open("logs.txt", ios::out | ios::app);
 
@@ -84,8 +86,7 @@ void logMessage(string message, string username)
     logs.close();
 }
 
-void logMeesage(string message, string filename, int codeline)
-{
+void logMeesage(string message, string filename, int codeline) {
     fstream logs;
     logs.open("logs.txt", ios::out | ios::app);
 
--- ej2/main.h
+++ ej2/main.h
@@ -7,8 +7,10 @@ enum LogLevel {
     INFO,
     WARNING,
     ERROR,
+    FATAL,
     CRITICAL,
-    SECURITY
+    SECURITY,
+    COUNT // Solo sirve para contar el numero de logs, siempre tiene que ser el ultimo
 };
 
 const string LOG_LEVEL_STRINGS[] = {
@@ -16,6 +18,7 @@ const string LOG_LEVEL_STRINGS[] = {
     "INFO", 
     "WARNING",
     "ERROR",
+    "FATAL (Throw Error)",
     "CRITICAL",
     "SECURITY"
 };
@@ -23,3 +26,5 @@ const string LOG_LEVEL_STRINGS[] = {
 void logMessage(string message, LogLevel level);
 void logMeesage(string message, string filename, int codeline);
 void logMessage(string message, string username);
+void handleSecurity();
+void handleMessage(LogLevel level);
